bfb92e66df6912eee9d966f8dc3e10cb57ccb668
pc1 <pc1@Alan>
2025-08-07 20:19:01 -0600

    Avance del menu

--- src/main/java/com/mycompany/proyectomenu1/ProyectoMenu1.java
+++ src/main/java/com/mycompany/proyectomenu1/ProyectoMenu1.java
@@ -12,51 +12,58 @@ import java.util.Vector;
  * @author pc1
  */
 public class ProyectoMenu1 {
-
+    static Scanner scanner=new Scanner(System.in);
     
     public static void main(String[] args) {
-        Vector<String> NombresPersonajes = new Vector<>();
-        Vector<String> ArmasPersonajes = new Vector<>();
+        Vector<String> NombrePersonajes = new Vector<>();
+        Vector<String> NombreArmas = new Vector<>();
         Vector<String> HabilidadesPersonajes = new Vector<>();
-        Scanner scanner=new Scanner(System.in);
         System.out.println("Gestion de Personajes");
         System.out.println("1. Agregar Personaje");
         System.out.println("2. Editar Personaje");
         System.out.println("3. Eliminar Personaje");
-        
+        System.out.println("4. Salir");
         
         System.out.println("Elige una opcion:");
-        int opcion=scanner.nextInt();
+        int opcion=0;
         
-        switch(opcion) {
-            case 1:
-                System.out.println("AGEGAR PERSONAJE");
-                System.out.println("1. Ingresar nombre del personaje");
-                    personaje();    
-                    
-                System.out.println("2. Ingresar arma del personaje");
-                    arma();
-                    
-                System.out.println("3. Ingresar habilidad del personaje");
-                    habilidad();
-                
-                System.out.println("4. Ingresar Nivel de poder del personaje");
-                    nivel();
-                        break;   
+        do{
+        opcion=scanner.nextInt();
+            switch(opcion) {
+                case 1 :
+                    do{
+                        System.out.println("Ingresar nombre del personaje");
+                        String nombre = scanner.nextLine();
+                        NombrePersonajes.add(nombre);
                         
-                        }
+                        System.out.println("Ingresar arma del personaje");
+                        String arma = scanner.nextLine();
+                        NombreArmas.add(arma);
+                        
+                        System.out.println("Ingresar habilidades del personaje del personaje");
+                        String habilidad = scanner.nextLine();
+                        HabilidadesPersonajes.add(habilidad);
+                        
+                        System.out.println("1. Agregar otro personaje?") ;
+                        System.out.println("2. Atras") ;
+                        
+                        break;
+        
+                    }while(opcion!=2) ;
+                
+                
+                
+            }
+            
+        }while(opcion!=4);
+        
+         
+        
+        
+                                
     }
 
-    private static void personaje() {
-        
-    }
-    private static void arma() {
-        
-    }
-    private static void habilidad() {
-        
-    }
-    private static void nivel() {
-        
-    }
+    
+    
+    
 }
