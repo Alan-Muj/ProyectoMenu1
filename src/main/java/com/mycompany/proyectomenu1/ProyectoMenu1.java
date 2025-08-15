e0fcf805abb0f053aafae0b58af27424835a2a74
pc1 <pc1@Alan>
2025-08-15 00:48:51 -0600

    Avance del menu

--- src/main/java/com/mycompany/proyectomenu1/ProyectoMenu1.java
+++ src/main/java/com/mycompany/proyectomenu1/ProyectoMenu1.java
@@ -5,7 +5,7 @@
 package com.mycompany.proyectomenu1;
 
 import java.util.Scanner;
-import java.util.Vector;
+
 
 /**
  *
@@ -15,55 +15,71 @@ public class ProyectoMenu1 {
     static Scanner scanner=new Scanner(System.in);
     
     public static void main(String[] args) {
-        Vector<String> NombrePersonajes = new Vector<>();
-        Vector<String> NombreArmas = new Vector<>();
-        Vector<String> HabilidadesPersonajes = new Vector<>();
-        System.out.println("Gestion de Personajes");
-        System.out.println("1. Agregar Personaje");
-        System.out.println("2. Editar Personaje");
-        System.out.println("3. Eliminar Personaje");
-        System.out.println("4. Salir");
-        
-        System.out.println("Elige una opcion:");
-        int opcion=0;
-        
-        do{
-        opcion=scanner.nextInt();
-            switch(opcion) {
-                case 1 :
-                    do{
-                        System.out.println("Ingresar nombre del personaje");
-                        String nombre = scanner.nextLine();
-                        NombrePersonajes.add(nombre);
-                        
-                        System.out.println("Ingresar arma del personaje");
-                        String arma = scanner.nextLine();
-                        NombreArmas.add(arma);
-                        
-                        System.out.println("Ingresar habilidades del personaje del personaje");
-                        String habilidad = scanner.nextLine();
-                        HabilidadesPersonajes.add(habilidad);
-                        
-                        System.out.println("1. Agregar otro personaje?") ;
-                        System.out.println("2. Atras") ;
-                        
-                        break;
-        
-                    }while(opcion!=2) ;
-                
-                
-                
-            }
+        int opcion = 0 ;
+        String[] NombrePersonaje = new String[25] ;
+        String [] NombreArmaPersonaje = new String [25] ;
+        String [] HabilidadesPersonaje = new String [25] ;
+     do{
+            System.out.println("Gestion de Personajes");
+            System.out.println("1. Agregar Personaje");
+            System.out.println("2. Editar Personaje");
+            System.out.println("3. Eliminar Personaje");
+            System.out.println("4. Salir");
+            System.out.println("Elege una opcion");
+            opcion = scanner.nextInt();
             
-        }while(opcion!=4);
-        
-         
-        
-        
+        switch(opcion) {
+            case 1 :
+                do{
+                    System.out.println("AGREGAR PERSONAJE");
+                    for(int i=0;i<1; i++){
+                        System.out.println("Ingresar nombre del personaje");
+                        NombrePersonaje[i] = scanner.next().strip() ;
+                    }
+                    
+                    for(int i=0;i<1; i++){
+                        System.out.println("Ingresar nombre del arma del personaje");
+                        NombreArmaPersonaje[i] = scanner.next().strip() ;
+                    }
+  
+                    for(int i=0;i<5; i++){
+                        System.out.println("Ingresar habilidades del personaje (Max. 5)");
+                        HabilidadesPersonaje[i] = scanner.next().strip() ;
+                    }
+                    
+                  System.out.println("El nombre del personaje");  
+                  for(int i=0;i<1; i++){
+                        System.out.println(NombrePersonaje[i]);
+                        NombrePersonaje[i] = scanner.next().strip() ;
+                    }
+                  
+                  System.out.println("El nombre del arma del personaje");  
+                  for(int i=0;i<1; i++){
+                        System.out.println(NombreArmaPersonaje[i]);
+                        NombrePersonaje[i] = scanner.next().strip() ;
+                    }
+                  
+                  System.out.println("Las habilidades del personaje");  
+                  for(int i=0;i<5; i++){
+                        System.out.println(HabilidadesPersonaje[i]);
+                        NombrePersonaje[i] = scanner.next().strip() ;
+                    }
+                  
+                  System.out.println("1. Agregar otro personaje");
+                    System.out.println("2. Retroceder");
+                    opcion = scanner.nextInt();
+                }while(opcion != 2) ; //Final de caso 1
+                
+                
+                
+                
+        } //Fin switch    
+            
+            
+        }while(opcion != 4) ;  //Fin do principal1
                                 
-    }
+    } // Final public static void
 
     
     
-    
-}
+} // Final public class
