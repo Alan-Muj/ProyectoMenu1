f4b95a47f23c8911f42612b92e9ff879de22f66e
pc1 <pc1@Alan>
2025-08-15 21:31:05 -0600

    Avance del menu 2

--- src/main/java/com/mycompany/proyectomenu1/ProyectoMenu1.java
+++ src/main/java/com/mycompany/proyectomenu1/ProyectoMenu1.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.proyectomenu1;
 
+
+import java.util.Random;
 import java.util.Scanner;
 
 
@@ -13,73 +15,100 @@ import java.util.Scanner;
  */
 public class ProyectoMenu1 {
     static Scanner scanner=new Scanner(System.in);
-    
+    Random random = new Random();
     public static void main(String[] args) {
         int opcion = 0 ;
+        int contador = 0 ;
+        int fuerza = 1 ;
+        String nombrebuscado;
+        
+        
         String[] NombrePersonaje = new String[25] ;
         String [] NombreArmaPersonaje = new String [25] ;
-        String [] HabilidadesPersonaje = new String [25] ;
+        String [][] HabilidadesPersonaje = new String [25][5] ;
+        int [] NivelFuerza = new int [25];
      do{
-            System.out.println("Gestion de Personajes");
+            System.out.println(" ---GESTION DE PERSONAJES--- ");
             System.out.println("1. Agregar Personaje");
             System.out.println("2. Editar Personaje");
             System.out.println("3. Eliminar Personaje");
-            System.out.println("4. Salir");
-            System.out.println("Elege una opcion");
+            System.out.println("4. Ver datos de un personaje");
+            System.out.println("5. Ver listado de personajes");
+            System.out.println("6. Realizar pelea entre personajes");
+            System.out.println("7. Ver historial de peleas");
+            System.out.println("8. Ver datos de estudiante");
+            System.out.println("9. Salir");
+            System.out.println("Elege una opcion:");
             opcion = scanner.nextInt();
             
         switch(opcion) {
             case 1 :
                 do{
-                    System.out.println("AGREGAR PERSONAJE");
-                    for(int i=0;i<1; i++){
-                        System.out.println("Ingresar nombre del personaje");
-                        NombrePersonaje[i] = scanner.next().strip() ;
-                    }
+             
+                    scanner.nextLine();
+                    System.out.println(" --- AGREGAR PERSONAJE --- ");
+                    String nombre;
+                    boolean nombreRepetido;
+                    do{
+                        nombreRepetido = false;                  
+                        System.out.println("Ingresar nombre del personaje:");
+                        nombre = scanner.nextLine().strip();
+                        for (int i = 0; i < contador; i++) {
+                                if (NombrePersonaje[i] != null && NombrePersonaje[i].equalsIgnoreCase(nombre)) {
+                                    nombreRepetido = true;
+                                    System.out.println("Nombre no disponible, Ingrese otro.");
+                                    break;
+                                }
+                            }
+                        } while (nombreRepetido);
+                        NombrePersonaje[contador] = nombre;
+                        
+                    scanner.nextLine();    
                     
-                    for(int i=0;i<1; i++){
-                        System.out.println("Ingresar nombre del arma del personaje");
-                        NombreArmaPersonaje[i] = scanner.next().strip() ;
+                        System.out.println("Ingresar nombre del arma del personaje:");
+                        NombreArmaPersonaje[contador] = scanner.nextLine().strip() ;   
+                      
+                    scanner.nextLine();    
+                    for(int j=0;j<5; j++){
+            
+                        System.out.println("Ingresar habilidades del personaje (Max. 5) ó 'Continuar' para finalizar");
+                        String Habilidad = scanner.nextLine().strip();
+                        if (Habilidad.equalsIgnoreCase("Continuar")) {
+                                break;
+                            }
+                        HabilidadesPersonaje[contador][j] = Habilidad;
                     }
-  
-                    for(int i=0;i<5; i++){
-                        System.out.println("Ingresar habilidades del personaje (Max. 5)");
-                        HabilidadesPersonaje[i] = scanner.next().strip() ;
-                    }
-                    
-                  System.out.println("El nombre del personaje");  
-                  for(int i=0;i<1; i++){
-                        System.out.println(NombrePersonaje[i]);
-                        NombrePersonaje[i] = scanner.next().strip() ;
-                    }
-                  
-                  System.out.println("El nombre del arma del personaje");  
-                  for(int i=0;i<1; i++){
-                        System.out.println(NombreArmaPersonaje[i]);
-                        NombrePersonaje[i] = scanner.next().strip() ;
-                    }
-                  
-                  System.out.println("Las habilidades del personaje");  
-                  for(int i=0;i<5; i++){
-                        System.out.println(HabilidadesPersonaje[i]);
-                        NombrePersonaje[i] = scanner.next().strip() ;
-                    }
-                  
-                  System.out.println("1. Agregar otro personaje");
+                    System.out.println("--PERSONAJE AGREGADO CORRECTAMENTE--");
+                    fuerza = new Random().nextInt(100);
+                    System.out.println("El nivel de fuerza es: " + fuerza);
+                    System.out.println("1. Agregar otro personaje");
                     System.out.println("2. Retroceder");
+                    NivelFuerza[contador] = scanner.nextInt();
+                    contador++;
                     opcion = scanner.nextInt();
                 }while(opcion != 2) ; //Final de caso 1
+            
+            case 2 :
+                do{
+                System.out.println("--- MODIFICAR PERSONAJE ---");
+                System.out.println("Ingresar nombre de personaje a modificar: "); 
+                nombrebuscado = scanner.nextLine().strip();
+                
+                int i = 0;
+                }while(opcion !=2) ;
                 
                 
                 
-                
+    
         } //Fin switch    
             
             
-        }while(opcion != 4) ;  //Fin do principal1
+        }while(opcion != 9) ;  //Fin do principal1
                                 
-    } // Final public static void
-
+    } // Final public static void main
+    
+   
+   
     
     
 } // Final public class
