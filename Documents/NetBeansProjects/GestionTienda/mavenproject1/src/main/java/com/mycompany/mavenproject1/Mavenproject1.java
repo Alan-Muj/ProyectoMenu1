c41540aca65afee9e9234591792d995d1d92cbc9
Alan Muj <2921131020709@ingenieria.usac.edu.gt>
2025-08-24 23:10:48 -0600

    Avance del menu tienda; credenciales

---
+++ Documents/NetBeansProjects/GestionTienda/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author pc1
+ */
+public class Mavenproject1 {
+    
+static Scanner scanner=new Scanner(System.in);
+int contador = 0 ;
+
+
+    public static void main(String[] args) {
+        String[] NombreUsuario = new String[25] ; //Vector para usuario
+        String[] CarnetUsuario = new String[25] ; //Vector para carnet usuario
+        int contador = 0 ;
+        
+        System.out.println("-----GESTION DE PRODUCTOS-----");
+        System.out.println("Ingresar nombre de ususario");
+        NombreUsuario [contador] = scanner.nextLine().strip() ;
+        System.out.println("Ingresar Canret");
+        CarnetUsuario [contador] = scanner.nextLine().strip() ;
+        
+    }//Fin public static void main
+}//Fin public class
