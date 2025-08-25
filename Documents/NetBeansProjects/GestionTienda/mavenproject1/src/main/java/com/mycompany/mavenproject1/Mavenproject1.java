fd4dcc2935e3877ab61bc011133de0016aaf9de0
Alan Muj <2921131020709@ingenieria.usac.edu.gt>
2025-08-24 23:16:23 -0600

    Avance del menu tienda; credenciales

--- Documents/NetBeansProjects/GestionTienda/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ Documents/NetBeansProjects/GestionTienda/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -20,12 +20,16 @@ int contador = 0 ;
         String[] NombreUsuario = new String[25] ; //Vector para usuario
         String[] CarnetUsuario = new String[25] ; //Vector para carnet usuario
         int contador = 0 ;
+        int opcion = 0 ;
         
         System.out.println("-----GESTION DE PRODUCTOS-----");
         System.out.println("Ingresar nombre de ususario");
         NombreUsuario [contador] = scanner.nextLine().strip() ;
         System.out.println("Ingresar Canret");
         CarnetUsuario [contador] = scanner.nextLine().strip() ;
+            do{
+                
+            }while(opcion != 9) ;
         
     }//Fin public static void main
 }//Fin public class
