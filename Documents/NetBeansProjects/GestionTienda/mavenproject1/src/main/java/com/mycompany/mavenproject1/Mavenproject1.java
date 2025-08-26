5d739f32938c6edb1f5311cd1a8a65789095277b
Alan Muj <2921131020709@ingenieria.usac.edu.gt>
2025-08-25 21:57:36 -0600

    Menu tienda; avance opcion 1 con mi primera clase :)

--- Documents/NetBeansProjects/GestionTienda/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ Documents/NetBeansProjects/GestionTienda/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -11,22 +11,26 @@ import java.util.Scanner;
  * @author pc1
  */
 public class Mavenproject1 {
+    static String Nombre ;
+    static String Categoria ;
+    static String Precio ;
+    static String Stock ;
+    static String CUnico ;
     
-static Scanner scanner=new Scanner(System.in);
-int contador = 0 ;
-
-
+    
+    static int contador = 0 ;    
+    static Scanner scanner=new Scanner(System.in);
+    static String[] NombreUsuario = new String[25] ; //Vector para usuario
+    static String[] CarnetUsuario = new String[25] ; //Vector para carnet usuario
+    static String[] NombreProducto = new String[25] ; //Vector para nombres de producto
+    static String[] CategoriaProducto = new String[25] ; //Vector para categorias de producto
+    static String[] Precios = new String[25] ; //Vector para precios producto
+    static String[] CantidadStock = new String[25] ; //Vector para stock producto
+    static String[] CodigoUnico = new String[25] ; //Vector para código único producto
+    static int opcion = 0 ; //Contador para vectores
+    
+    
     public static void main(String[] args) {
-        String[] NombreUsuario = new String[25] ; //Vector para usuario
-        String[] CarnetUsuario = new String[25] ; //Vector para carnet usuario
-        String[] NombreProducto = new String[25] ; //Vector para nombres de producto
-        String[] CategoriaProducto = new String[25] ; //Vector para categorias de producto
-        String[] Precios = new String[25] ; //Vector para precios producto
-        String[] Stock = new String[25] ; //Vector para stock producto
-        String[] CodigoUnico = new String[25] ; //Vector para código único producto
-        int contador = 0 ;
-        int opcion = 0 ;
-        
         System.out.println("-----GESTION DE PRODUCTOS-----");
         System.out.println("Ingresar nombre de ususario");
         NombreUsuario [contador] = scanner.nextLine().strip() ;
@@ -44,23 +48,56 @@ int contador = 0 ;
                 System.out.println("8. Salir");
                 opcion = scanner.nextInt() ;
             switch (opcion) {
-                case 1:
-                    System.out.println("---AGREGAR PRODUCTO---");
-                    System.out.println("Ingesar Nombre del Producto");
-                    NombreProducto [contador] = scanner.nextLine().strip();
-                    System.out.println("Ingresar Categoría del producto");
-                    CategoriaProducto [contador] = scanner.nextLine().strip() ;
-                    System.out.println("Ingresar Precio del prducto");
-                    Precios [contador] = scanner.nextLine().strip() ;
-                    System.out.println("Ingresar cantidad en Stock");
-                    Stock [contador] = scanner.nextLine().strip() ;
-                    System.out.println("Ingresar Código Único del Producto");
-                    CodigoUnico [contador] = scanner.nextLine().strip();                  
-            }    
+                case 1:                    
+                        Caso1 () ; 
+                       
+                        
+                        
+            }  //Fin switch  
 
 
                 
             }while(opcion != 8) ;
         
     }//Fin public static void main
+    
+    //Metodo caso 1
+    static public void Caso1 ()  { //Inicio caso 1
+        do{
+                    scanner.nextLine();
+                    System.out.println("---AGREGAR PRODUCTO---");
+         
+                    System.out.println("Ingesar Nombre del Producto");                   
+                    NombreProducto [contador] = scanner.nextLine().strip();
+                   
+                    System.out.println("Ingresar Categoría del producto");
+                    CategoriaProducto [contador] = scanner.nextLine().strip() ;
+                    
+                    System.out.println("Ingresar Precio del prducto");
+                    Precios [contador] = scanner.nextLine().strip() ;
+                    
+                    System.out.println("Ingresar cantidad en Stock");
+                    CantidadStock [contador] = scanner.nextLine().strip() ;
+                    
+                    System.out.println("Ingresar Código Único del Producto");
+                    CodigoUnico [contador] = scanner.nextLine().strip();
+                    
+                    System.out.println("1. Agregar otro Producto");
+                    System.out.println("2. Retroceder");
+                    opcion = scanner.nextInt() ;
+                    
+        }while(opcion != 2) ;        
+                    
+} //Fin caso 1
+    
+    
+    
+    
 }//Fin public class
+
+
+ 
+
+
+
+
