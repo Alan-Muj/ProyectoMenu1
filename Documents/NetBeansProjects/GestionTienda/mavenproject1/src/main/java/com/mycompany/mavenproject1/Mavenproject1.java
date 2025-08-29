b39b48caf32a3020d1c28e6666591fa1a09deda4
Alan Muj <2921131020709@ingenieria.usac.edu.gt>
2025-08-28 23:01:11 -0600

    Menu tienda; avance opcion 1 Completada :))

--- Documents/NetBeansProjects/GestionTienda/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ Documents/NetBeansProjects/GestionTienda/mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -4,6 +4,7 @@
 
 package com.mycompany.mavenproject1;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -13,10 +14,9 @@ import java.util.Scanner;
 public class Mavenproject1 {
     static String Nombre ;
     static String Categoria ;
-    static String Precio ;
-    static String Stock ;
-    static String CUnico ;
-    
+    static double Precio ;
+    static double Stock ;
+    static String CUnico ; //Variable para codigo repetido
     
     static int contador = 0 ;    
     static Scanner scanner=new Scanner(System.in);
@@ -24,64 +24,96 @@ public class Mavenproject1 {
     static String[] CarnetUsuario = new String[25] ; //Vector para carnet usuario
     static String[] NombreProducto = new String[25] ; //Vector para nombres de producto
     static String[] CategoriaProducto = new String[25] ; //Vector para categorias de producto
-    static String[] Precios = new String[25] ; //Vector para precios producto
-    static String[] CantidadStock = new String[25] ; //Vector para stock producto
+    static double[] Precios = new double[25] ; //Vector para precios producto
+    static double[] CantidadStock = new double[25] ; //Vector para stock producto
     static String[] CodigoUnico = new String[25] ; //Vector para código único producto
     static int opcion = 0 ; //Contador para vectores
     
     
     public static void main(String[] args) {
+        
         System.out.println("-----GESTION DE PRODUCTOS-----");
         System.out.println("Ingresar nombre de ususario");
         NombreUsuario [contador] = scanner.nextLine().strip() ;
-        System.out.println("Ingresar Canret");
+        System.out.println("Ingresar Carnet");
         CarnetUsuario [contador] = scanner.nextLine().strip() ;
             do{
-                System.out.println("-----MENU GESTIÓN DE PRODUCTOS-----");
+                System.out.println("-----MENU GESTION DE PRODUCTOS-----");
                 System.out.println("1. Agregar Producto");
                 System.out.println("2. Bucar Producto");
                 System.out.println("3. Eliminar Producto");
                 System.out.println("4. Registrar Venta");
                 System.out.println("5. Generar Reportes");
                 System.out.println("6. Ver Datos del Estudiante");
-                System.out.println("7. Bitácora");
+                System.out.println("7. Bitacora");
                 System.out.println("8. Salir");
                 opcion = scanner.nextInt() ;
             switch (opcion) {
                 case 1:                    
-                        Caso1 () ; 
+                        Caso1 () ;
+                        
+                    
                        
-                        
-                        
+               
             }  //Fin switch  
 
-
+            
+            
                 
             }while(opcion != 8) ;
         
     }//Fin public static void main
     
     //Metodo caso 1
+    @SuppressWarnings("empty-statement")
     static public void Caso1 ()  { //Inicio caso 1
         do{
                     scanner.nextLine();
                     System.out.println("---AGREGAR PRODUCTO---");
-         
-                    System.out.println("Ingesar Nombre del Producto");                   
+                    
+                    System.out.println("Ingresar Nombre del Producto");                   
                     NombreProducto [contador] = scanner.nextLine().strip();
                    
                     System.out.println("Ingresar Categoría del producto");
                     CategoriaProducto [contador] = scanner.nextLine().strip() ;
                     
-                    System.out.println("Ingresar Precio del prducto");
-                    Precios [contador] = scanner.nextLine().strip() ;
+                    do {
+                    System.out.println("Ingresar Precio del producto");
+                    Precio = scanner.nextDouble() ;
+                   if ( Precio < 0) {
+                       System.out.println("EL PRECIO NO PUEDE SER NEGATIVO");
+                    } else {
+                       Precios [contador] = Precio ;
+                          }
+                    }while (Precio < 0) ;
+                    scanner.nextLine(); 
                     
+                    do {
                     System.out.println("Ingresar cantidad en Stock");
-                    CantidadStock [contador] = scanner.nextLine().strip() ;
-                    
-                    System.out.println("Ingresar Código Único del Producto");
-                    CodigoUnico [contador] = scanner.nextLine().strip();
-                    
+                    Stock = scanner.nextDouble() ;
+                   if ( Stock < 0) {
+                       System.out.println("EL CANTIDAD DE STOCK NO PUEDE SER NEGATIVA");
+                    } else {
+                       CantidadStock [contador] = Stock ;
+                          }
+                    }while (Stock < 0) ;
+                    scanner.nextLine();                 
+                     
+                    boolean CodigoRepetido;
+                    do{
+                        CodigoRepetido = false;                  
+                        System.out.println("Ingresar Codgio Unico del Producto:");
+                        CUnico = scanner.nextLine().strip();
+                        for (int i = 0; i < contador; i++) {
+                                if (CodigoUnico[i] != null && CodigoUnico[i].equalsIgnoreCase(CUnico)) {
+                                    CodigoRepetido = true;
+                                    System.out.println("CODIGO UNICO REPETIDO");
+                                    break;
+                                }
+                            }
+                        } while (CodigoRepetido);
+                        CodigoUnico[contador] = CUnico;
+                    contador++;
                     System.out.println("1. Agregar otro Producto");
                     System.out.println("2. Retroceder");
                     opcion = scanner.nextInt() ;
